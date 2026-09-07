bf979a2536a84589144c92d9ae23cdc91c898ee9
Gx-Droid <carloseduardoyt500@gmail.com>
2026-09-07 05:26:17 -0300

    Refactor TitleState.hx for improved readability

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -3,6 +3,7 @@ package;
 #if desktop
 import sys.thread.Thread;
 #end
+
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -14,12 +15,13 @@ import flixel.addons.transition.TransitionData;
 import haxe.Json;
 import openfl.display.Bitmap;
 import openfl.display.BitmapData;
+
 #if MODS_ALLOWED
 import sys.FileSystem;
 import sys.io.File;
 #end
+
 import options.GraphicsSettingsSubState;
-//import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.frames.FlxFrame;
 import flixel.group.FlxGroup;
@@ -37,9 +39,9 @@ import flixel.util.FlxTimer;
 import openfl.Assets;
 
 using StringTools;
+
 typedef TitleData =
 {
-
 	titlex:Float,
 	titley:Float,
 	startx:Float,
@@ -49,6 +51,7 @@ typedef TitleData =
 	backgroundSprite:String,
 	bpm:Int
 }
+
 class TitleState extends MusicBeatState
 {
 	public static var muteKeys:Array<FlxKey> = [FlxKey.ZERO];
@@ -73,7 +76,7 @@ class TitleState extends MusicBeatState
 		#if LUA_ALLOWED
 		Paths.pushGlobalMods();
 		#end
-		// Just to load a mod on start up if ya got one. For mods that change the menu music and bg
+
 		WeekData.loadTheFirstEnabledMod();
 
 		FlxG.game.focusLostFramerate = 60;
@@ -84,8 +87,6 @@ class TitleState extends MusicBeatState
 
 		PlayerSettings.init();
 
-		// DEBUG BULLSHIT
-
 		super.create();
 
 		FlxG.save.bind('funkin', CoolUtil.getSavePath());
@@ -94,19 +95,23 @@ class TitleState extends MusicBeatState
 
 		Highscore.load();
 
-		if (FileSystem.isDirectory(Sys.getEnv("AppData") + "/shadowmario/FunkinAtFreddys")) {
-			funkinatfreddysExist = true; //checks if u played tghe mod before
-		}
-
-		if(!initialized)
+		#if desktop
+		if (FileSystem.isDirectory(Sys.getEnv("AppData") + "/shadowmario/FunkinAtFreddys"))
 		{
-			if(FlxG.save.data != null && FlxG.save.data.fullscreen)
+			funkinatfreddysExist = true;
+		}
+		#end
+
+		if (!initialized)
+		{
+			if (FlxG.save.data != null && FlxG.save.data.fullscreen)
 			{
 				FlxG.fullscreen = FlxG.save.data.fullscreen;
-				//trace('LOADED FULLSCREEN SETTING!!');
 			}
+
 			FlxG.signals.preStateSwitch.add(Conductor.clearAll);
 			FlxG.signals.preStateSwitch.add(MusicBeatState.clearShaderData);
+
 			persistentUpdate = true;
 			persistentDraw = true;
 		}
@@ -117,18 +122,30 @@ class TitleState extends MusicBeatState
 		}
 
 		FlxG.mouse.visible = false;
+
 		#if FREEPLAY
+
 		MusicBeatState.switchState(new FreeplayState());
+
 		#elseif CHARTING
+
 		MusicBeatState.switchState(new ChartingState());
+
 		#else
-		if(FlxG.save.data.flashing == null && !FlashingState.leftState) {
+
+		if (FlxG.save.data.flashing == null && !FlashingState.leftState)
+		{
 			FlxTransitionableState.skipNextTransIn = true;
 			FlxTransitionableState.skipNextTransOut = true;
+
 			MusicBeatState.switchState(new FlashingState());
-		} else {
+		}
+		else
+		{
 			if (initialized)
+			{
 				startIntro();
+			}
 			else
 			{
 				new FlxTimer().start(1, function(tmr:FlxTimer)
@@ -137,6 +154,7 @@ class TitleState extends MusicBeatState
 				});
 			}
 		}
+
 		#end
 	}
 
@@ -146,79 +164,142 @@ class TitleState extends MusicBeatState
 	{
 		if (!initialized)
 		{
-			if(FlxG.sound.music == null) {
-
+			if (FlxG.sound.music == null)
+			{
 				FlxG.sound.playMusic(Paths.music('title'), 0);
-				FlxG.sound.music.fadeIn(1,0,0.8);
+				FlxG.sound.music.fadeIn(1, 0, 0.8);
 			}
 		}
-
 
 		Conductor.changeBPM(130);
 		persistentUpdate = true;
 
+		var bg:FlxSprite =
+			new FlxSprite().makeGraphic(
+				FlxG.width,
+				FlxG.height,
+				FlxColor.BLACK
+			);
 
-
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 
-		var ourp = new FlxSprite().loadGraphic(Paths.image('ourp'));
-		ourp.scale.set(2.5,2.5);
+		var ourp:FlxSprite =
+			new FlxSprite().loadGraphic(Paths.image('ourp'));
+
+		ourp.scale.set(2.5, 2.5);
 		ourp.updateHitbox();
-		ourp.setPosition(FlxG.width-ourp.width - 100,(FlxG.height-ourp.height)/2);
+
+		ourp.setPosition(
+			FlxG.width - ourp.width - 100,
+			(FlxG.height - ourp.height) / 2
+		);
+
 		add(ourp);
 
-		var enterTxt = new FlxText(150,600,0,'PRESS ENTER TO BEGIN');
-		enterTxt.setFormat(Paths.font("options.ttf"), 40, 0xFFB6B6B6, CENTER,FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		var enterTxt:FlxText =
+			new FlxText(
+				150,
+				600,
+				0,
+				'PRESS ENTER TO BEGIN'
+			);
+
+		enterTxt.setFormat(
+			Paths.font("options.ttf"),
+			40,
+			0xFFB6B6B6,
+			CENTER,
+			FlxTextBorderStyle.OUTLINE,
+			FlxColor.BLACK
+		);
+
 		add(enterTxt);
 
 		logoBl = new FlxSprite();
-		logoBl.frames = Paths.getSparrowAtlas('logobump');
-		logoBl.animation.addByPrefix('bump', 'logobump idle', 12, false);
+
+		logoBl.frames =
+			Paths.getSparrowAtlas('logobump');
+
+		logoBl.animation.addByPrefix(
+			'bump',
+			'logobump idle',
+			12,
+			false
+		);
+
 		logoBl.animation.play('bump');
-		logoBl.scale.set(0.75,0.75);
+
+		logoBl.scale.set(0.75, 0.75);
 		logoBl.updateHitbox();
-		logoBl.setPosition(-125,-150);
+		logoBl.setPosition(-125, -150);
+
 		add(logoBl);
 
-		var statics = new FlxSprite(-150);
-		statics.frames = Paths.getSparrowAtlas('VHS');
-		statics.animation.addByPrefix('s','VHS',12);
+		var statics:FlxSprite =
+			new FlxSprite(-150);
+
+		statics.frames =
+			Paths.getSparrowAtlas('VHS');
+
+		statics.animation.addByPrefix(
+			's',
+			'VHS',
+			12
+		);
+
 		statics.animation.play('s');
-		statics.setGraphicSize(FlxG.width + 300,FlxG.height + 300);
+
+		statics.setGraphicSize(
+			FlxG.width + 300,
+			FlxG.height + 300
+		);
+
 		statics.updateHitbox();
+
 		statics.blend = ADD;
+
 		add(statics);
 
-		// FlxG.cameras.fade(FlxColor.BLACK,3,true);
-		var black = new FlxSprite().makeGraphic(FlxG.width,FlxG.height,FlxColor.BLACK);
+		var black:FlxSprite =
+			new FlxSprite().makeGraphic(
+				FlxG.width,
+				FlxG.height,
+				FlxColor.BLACK
+			);
+
 		add(black);
-		FlxTween.tween(black, {alpha: 0},4, {startDelay: 0.5});
+
+		FlxTween.tween(
+			black,
+			{alpha: 0},
+			4,
+			{startDelay: 0.5}
+		);
 
 		if (initialized)
 			skipIntro();
 		else
 			initialized = true;
-
-
-
 	}
 
-
 	var transitioning:Bool = false;
-	
 	var newTitle:Bool = false;
 	var titleTimer:Float = 0;
 
 	override function update(elapsed:Float)
 	{
 		if (FlxG.sound.music != null)
-			Conductor.songPosition = FlxG.sound.music.time;
-		// FlxG.watch.addQuick('amp', FlxG.sound.music.amplitude);
+		{
+			Conductor.songPosition =
+				FlxG.sound.music.time;
+		}
 
-		var pressedEnter:Bool = FlxG.keys.justPressed.ENTER || controls.ACCEPT;
+		var pressedEnter:Bool =
+			FlxG.keys.justPressed.ENTER ||
+			controls.ACCEPT;
 
 		#if mobile
+
 		for (touch in FlxG.touches.list)
 		{
 			if (touch.justPressed)
@@ -226,9 +307,11 @@ class TitleState extends MusicBeatState
 				pressedEnter = true;
 			}
 		}
+
 		#end
 
-		var gamepad:FlxGamepad = FlxG.gamepads.lastActive;
+		var gamepad:FlxGamepad =
+			FlxG.gamepads.lastActive;
 
 		if (gamepad != null)
 		{
@@ -236,47 +319,61 @@ class TitleState extends MusicBeatState
 				pressedEnter = true;
 
 			#if switch
+
 			if (gamepad.justPressed.B)
 				pressedEnter = true;
+
 			#end
 		}
-		
-		if (newTitle) {
-			titleTimer += CoolUtil.boundTo(elapsed, 0, 1);
-			if (titleTimer > 2) titleTimer -= 2;
-		}
 
-		// EASTER EGG
+		if (newTitle)
+		{
+			titleTimer +=
+				CoolUtil.boundTo(elapsed, 0, 1);
+
+			if (titleTimer > 2)
+				titleTimer -= 2;
+		}
 
 		if (initialized && !transitioning && skippedIntro)
 		{
 			if (newTitle && !pressedEnter)
 			{
 				var timer:Float = titleTimer;
+
 				if (timer >= 1)
 					timer = (-timer) + 2;
-				
-				timer = FlxEase.quadInOut(timer);
-				
-			}
-			
-			if(pressedEnter)
-			{
-				
 
-				FlxG.camera.flash(ClientPrefs.flashing ? FlxColor.WHITE : 0x4CFFFFFF, 1);
-				FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+				timer = FlxEase.quadInOut(timer);
+			}
+
+			if (pressedEnter)
+			{
+				FlxG.camera.flash(
+					ClientPrefs.flashing
+						? FlxColor.WHITE
+						: 0x4CFFFFFF,
+					1
+				);
+
+				FlxG.sound.play(
+					Paths.sound('confirmMenu'),
+					0.7
+				);
 
 				transitioning = true;
-				// FlxG.sound.music.stop();
 
-				new FlxTimer().start(1, function(tmr:FlxTimer)
-				{
+				new FlxTimer().start(
+					1,
+					function(tmr:FlxTimer)
+					{
+						MusicBeatState.switchState(
+							new MainMenuState()
+						);
 
-					MusicBeatState.switchState(new MainMenuState());
-					closedState = true;
-				});
-				// FlxG.sound.play(Paths.music('titleShoot'), 0.7);
+						closedState = true;
+					}
+				);
 			}
 		}
 
@@ -288,34 +385,32 @@ class TitleState extends MusicBeatState
 		super.update(elapsed);
 	}
 
+	private var sickBeats:Int = 0;
 
-
-	private var sickBeats:Int = 0; //Basically curBeat but won't be skipped if you hold the tab or resize the screen
 	public static var closedState:Bool = false;
+
 	override function beatHit()
 	{
 		super.beatHit();
 
-		if(logoBl != null)
+		if (logoBl != null)
 			logoBl.animation.play('bump', true);
 
-
-		if(!closedState) {
+		if (!closedState)
+		{
 			sickBeats++;
+
 			switch (sickBeats)
 			{
 				case 1:
-					// FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
-					// FlxG.sound.music.fadeIn(4, 0, 0.7);
-
 					skipIntro();
-
 			}
 		}
 	}
 
 	var skippedIntro:Bool = false;
 	var increaseVolume:Bool = false;
+
 	function skipIntro():Void
 	{
 		if (!skippedIntro)
@@ -323,4 +418,4 @@ class TitleState extends MusicBeatState
 			skippedIntro = true;
 		}
 	}
-}
+			}
